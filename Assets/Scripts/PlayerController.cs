dbc4a1fb89706c56de8dc79b6cfdb61fdeccbecf
Felipe Lefevre Marino <felipemarino91@gmail.com>
2016-10-10 16:37:47 -0300

    Player controller and colliders

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D rb2d;
+
+	void Start() {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	//update (called before rendering a frame) or fixedUpdate (before performing any physics calculation)
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		rb2d.AddForce (new Vector2 (moveHorizontal, moveVertical) * speed);
+	}
+} 
