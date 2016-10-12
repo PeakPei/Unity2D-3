94e69079fda0cffc7649e8b2192cf23cde7b27c5
marinofelipe <felipemarino91@gmail.com>
2016-10-12 15:04:40 -0300

    Camera Controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start ()
+	{
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Runs every frame just like Update, however it only runs after all items have been processed on update.
+	void LateUpdate ()
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
