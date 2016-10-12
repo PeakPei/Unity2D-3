7a544512e1dcde098d5df5e1b12ed5822cfcc22f
marinofelipe <felipemarino91@gmail.com>
2016-10-12 20:26:21 -0300

    Collectable rotating GameObjects

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+
+
+	
+	// Update is called once per frame; used in this case cause we are not using forces
+	void Update ()
+	{
+		transform.Rotate (new Vector3 (0, 0, 45) * Time.deltaTime);
+	}
+}
