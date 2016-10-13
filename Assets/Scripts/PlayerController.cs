c0420a9a97aac9b86e974a17d19d5fcac757eb46
marinofelipe <felipemarino91@gmail.com>
2016-10-12 23:13:56 -0300

    Counting collectables and displaying score

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
+
 	private Rigidbody2D rb2d;
+	private int collectedCount;
 
 	void Start ()
 	{
 		rb2d = GetComponent<Rigidbody2D> ();
+		collectedCount = 0;
+		winText.text = "";
+		setCountText ();
 	}
 
 	//update (called before rendering a frame) or fixedUpdate (before performing any physics calculation)
@@ -25,5 +33,15 @@ public class PlayerController : MonoBehaviour
 	{
 		if (other.gameObject.CompareTag ("PickUp"))
 			other.gameObject.SetActive (false);
+
+		collectedCount += 1;
+		setCountText ();
+	}
+
+	private  void setCountText ()
+	{
+		countText.text = "Count: " + collectedCount.ToString ();
+		if (collectedCount >= 12)
+			winText.text = "You win";
 	}
 }
