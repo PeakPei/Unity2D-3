ba11212ef20a7e3fd4e965a50fdfe7f3bf638c91
marinofelipe <felipemarino91@gmail.com>
2016-10-12 21:13:59 -0300

    PickUps as Trigger

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,29 @@
-﻿ using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
 	public float speed;
 	private Rigidbody2D rb2d;
 
-	void Start() {
+	void Start ()
+	{
 		rb2d = GetComponent<Rigidbody2D> ();
 	}
 
 	//update (called before rendering a frame) or fixedUpdate (before performing any physics calculation)
-	void FixedUpdate() {
-		float moveHorizontal = Input.GetAxis("Horizontal");
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 		rb2d.AddForce (new Vector2 (moveHorizontal, moveVertical) * speed);
 	}
-} 
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("PickUp"))
+			other.gameObject.SetActive (false);
+	}
+}
